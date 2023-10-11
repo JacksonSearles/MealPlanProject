2ff6ddfdb78851e9c285f0d226d66b2f691219c4
smontes2 <smontes2@binghamton.edu>
2023-10-11 16:11:55 -0400

    updated main

--- app.py
+++ app.py
@@ -89,5 +89,4 @@ def calculate_daily_spending():
     daily_budget = meal_plan_balance / days_left
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0')
