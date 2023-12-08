eae00889d32e2b3188b61275dd7f2082e0544842
sambuckler1 <120116786+sambuckler1@users.noreply.github.com>
2023-12-07 19:20:10 -0500

    comments

--- app.py
+++ app.py
@@ -28,33 +28,24 @@ def logged_in():
     global days_left
     global daily_budget
     meal_plan_balance = 0
-
-    #######################################
-    #When login button is clicked on sign in page, stores username and password from form element in html. 
-    #When you click login button, celenium code in next block executes.
-    if request.method == 'POST':
+    if request.method == 'POST': 
         username = request.form.get('username')
         password = request.form.get('password')
 
-    
     if request.method == "PUT":
         return redirect(url_for('login'))
-    #######################################
 
-    #######################################
-    #Celenium opens headless incognito browser withe the url of mealplan site
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     browser = webdriver.Chrome(options=options)
     browser.get('https://bing.campuscardcenter.com/ch/login.html')
 
-    #Takes username and password gathered from Flask POST method, and sends keys to actual mealplan Binghamton site
     elem = browser.find_element(By.NAME, 'username')
     elem.send_keys(username)
     elem = browser.find_element(By.NAME, 'password')
     elem.send_keys(password + Keys.RETURN)
-    #########################################
 
     try:      
         if browser.find_element(By.ID, 'welcome'):
@@ -64,11 +55,13 @@ def logged_in():
             words = soup.label.text
             first_name = words.split()  
 
-            target_strings = ["Resident Holding - Carryover", "BUCS", "Meal Plan C"]
-            body_content = soup.find("body").get_text()
-            positions = {target: body_content.find(target) for target in target_strings}
+            #Problem: Website formats the balances in different order for different people
+            #This block determines the order of the mealplan balances for the user, so it knows which balances to accumulate
+            target_strings = ["Resident Holding - Carryover", "BUCS", "Meal Plan C"] 
+            body_content = soup.find("body").get_text() 
+            positions = {target: body_content.find(target) for target in target_strings} 
 
-            sorted_targets = sorted(positions.keys(), key=lambda x: positions[x])
+            sorted_targets = sorted(positions.keys(), key=lambda x: positions[x]) 
             order = [sorted_targets.index(target) for target in target_strings]
 
             elements = soup.find_all(align="right")
@@ -81,8 +74,8 @@ def logged_in():
                     idx1 = e_string.index(sub1)
                     idx2 = e_string.index(sub2)
                     result = e_string[idx1 + len(sub1) + 3: idx2]
-                    if i == order[0] or i == order[2]:
-                        meal_plan_balance += float(result)
+                    if i == order[0] or i == order[2]: ##This checks to see if the balance being scanned is the one we care about
+                        meal_plan_balance += float(result) #if so, add to result
                     i += 1
             calculate_daily_spending()
             return render_template('userPage.html', first_name = first_name[2], balance=meal_plan_balance, days=days_left, budget=daily_budget)
