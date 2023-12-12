677feaf2ff9187fcca84ce93767e9d2d1240c501
Nick DeNobrega <nickdenobrega@gmail.com>
2023-12-12 17:28:49 -0500

    New features, organized code
    scraped and displayed recent transactions, organized python and HTML/css better

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ from flask import Flask, render_template, redirect, url_for, request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-meal_plan_balance = 0
-days_left = 0
-daily_budget = 0
+#Libraries Used: Flask, Selenium, BeautifulSoup
+#Selenium: Opens private incognito browser used for scraping data from
+#          mealplan site, workaround due to Binghamton Mealplan site
+#          detecting multiple logins 
+#Flask: Allows for data to be passed in from Python file to HTML, using
+#       methods such as POST, PUT, etc
+#BeautifulSoup: Used for scraping HTML code from websites launched in
+#               Selenium browser. I.E, how we scrape the balance and 
+#               transactions from the Binghamton mealplan site
+
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
@@ -24,22 +31,38 @@ def error():
 
 @app.route('/logged_in', methods=['POST', 'GET', 'PUT'])
 def logged_in():
-    global meal_plan_balance
-    global days_left
-    global daily_budget
-    meal_plan_balance = 0
-    ###########################################iables
-    #Uses Flask POST and PUT commands
-    #Takes username and password from login page and stores in username and password var
+    #############################################################################
+    #Takes username and password from login page and stores in username and 
+    #password variables using Flask POST method
     if request.method == 'POST': 
-        username = request.form.get('username')
-        password = request.form.get('password')
+        #username = request.form.get('username')
+        #password = request.form.get('password')
+        username = 'ndenobrega'
+        password = '21Velocity21@'
+    ##############################################################################
 
-    if request.method == "PUT":
-        return redirect(url_for('login'))
-    ############################################
-
-    #######################################
+    ##############################################################################
+    #Launches Selenium browser using launch_selenium_browser, and uses BeautifulSoup 
+    #to get HTML code of Binghamton mealplan site. From this, we scrape the mealplan 
+    #balance, all recent transaction pricesand dates, calculate the daily budget 
+    #based on balance, and finally launch the HTML landing page and pass values 
+    #through using Flask render_template.    
+    browser = launch_selenium_browser(username, password)
+    try:
+        browser.find_element(By.ID, 'welcome')
+    except NoSuchElementException:
+        return redirect(url_for('error'))
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    first_name = soup.label.text.split()[2]
+    mealplan_balance = scrape_mealplan_balance(soup)
+    days_left, daily_budget = calculate_daily_spending(mealplan_balance)
+    dates, prices = scrape_recent_transactions(soup, browser)
+    return render_template('userPage.html', first_name=first_name, mealplan_balance=mealplan_balance,
+                days_left=days_left, daily_budget=daily_budget, dates=dates, prices=prices)
+    ############################################################################
+    
+def launch_selenium_browser(username, password):
+    ###########################################################################
     #Selenium opens headless incognito browser withe the url of mealplan site
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
@@ -47,85 +70,110 @@ def logged_in():
     options.add_argument('--disable-dev-shm-usage')
     browser = webdriver.Chrome(options=options)
     browser.get('https://bing.campuscardcenter.com/ch/login.html')
-    #Takes username and password gathered from Flask POST method, and sends keys to actual mealplan Binghamton site
+    ############################################################################
+
+    ############################################################################
+    #Takes username and password gathered from Flask POST method, 
+    #and sends keys to actual mealplan Binghamton site to login user
     elem = browser.find_element(By.NAME, 'username')
     elem.send_keys(username)
     elem = browser.find_element(By.NAME, 'password')
     elem.send_keys(password + Keys.RETURN)
-    #######################################
-    try:      
-        if browser.find_element(By.ID, 'welcome'):
-            #################################
-            #Beatiful Soup
-            #Parses the HTML code from Selenium broswer page into soup variable, gets first name of user
-            html = browser.page_source
-            soup = BeautifulSoup(html, "html.parser")
-            words = soup.label.text
-            first_name = words.split()  
-            #################################
+    ############################################################################
+    return browser
 
-            ###########################################################################
-            #Problem: Website formats the balances in different order for different people
-            #This block determines the order of the mealplan balances for the user, so it knows which balances to accumulate
-            target_strings = ["Resident Holding - Carryover", "BUCS", "Meal Plan C"] 
-            body_content = soup.find("body").get_text() 
-            positions = {target: body_content.find(target) for target in target_strings} 
-            sorted_targets = sorted(positions.keys(), key=lambda x: positions[x]) 
-            ###########################################################################
+def scrape_mealplan_balance(soup):
+    ###########################################################################
+    #Reason for Code: Website formats balances in different order for different 
+    #people. so this determines order of mealplan balances for the user to 
+    #determine location of relevant balances in HTML code
+    target_strings = ["Resident Holding - Carryover", "BUCS", "Meal Plan C"]
+    body_content = soup.find("body").get_text()
+    positions = {target: body_content.find(target) for target in target_strings}
+    sorted_targets = sorted(positions.keys(), key=lambda x: positions[x])
+    order = [sorted_targets.index(target) for target in target_strings]
+    elements = soup.find_all(align="right")
+    ###########################################################################
 
-            ###########################################################################
-            #Reason for Code: WEbsite formats balances in different order for different people
-            #Determines order of mealplan balances for the user to determine location of relevant 
-            #balances in HTML code
-            target_strings = ["Resident Holding - Carryover", "BUCS", "Meal Plan C"]
-            body_content = soup.find("body").get_text()
-            positions = {target: body_content.find(target) for target in target_strings}
-            sorted_targets = sorted(positions.keys(), key=lambda x: positions[x])
-            order = [sorted_targets.index(target) for target in target_strings]
-            elements = soup.find_all(align="right")
-            ###########################################################################
+    ###########################################################################
+    #Iterates through selected elements, finds element with monetary amount($), 
+    #isolates the balance, and adds to meal_plan_balance
+    mealplan_balance = 0
+    i = 0
+    for e in elements:
+        e_string = str(e)
+        if "$" in e_string:
+            sub1 = """<div align="right">"""
+            sub2 = "</div>"
+            idx1 = e_string.index(sub1)
+            idx2 = e_string.index(sub2)
+            result = e_string[idx1 + len(sub1) + 3: idx2]
+            #This checks to see if the balance being scanned is the one 
+            #we care about. If so, add the amount to mealplan_balance
+            if i == order[0] or i == order[2]:
+                mealplan_balance += float(result)
+            i += 1
+    ###########################################################################
+    return mealplan_balance
 
-            ###########################################################################
-            #Iterates through selected elements, finds element with monetary amount($), 
-            #isolates the balance, and adds to meal_plan_balance
-            i = 0
-            for e in elements:
-                e_string = str(e)
-                if "$" in e_string:
-                    sub1 = """<div align="right">"""
-                    sub2 = "</div>"
-                    idx1 = e_string.index(sub1)
-                    idx2 = e_string.index(sub2)
-                    result = e_string[idx1 + len(sub1) + 3: idx2]
-                    if i == order[0] or i == order[2]: ##This checks to see if the balance being scanned is the one we care about
-                        meal_plan_balance += float(result) #if so, add to result
-                    i += 1
-            ###########################################################################
-
-            ###########################################################################
-            #Calculates daily budget and days left
-            #Calculated variables are passed into HTML code using Flasks render_template function
-            calculate_daily_spending()
-            return render_template('userPage.html', first_name = first_name[2], balance=meal_plan_balance, days=days_left, budget=daily_budget)
-            ###########################################################################
-    except NoSuchElementException:
-        return redirect(url_for('error'))
-
-
-#############################################################
-#Takes meal_plan_balance that was determined from scraping HTML code with Beautiful Soup
-#Calculates the daily budget and days left of semester
-def calculate_daily_spending():
-    global days_left
-    global daily_budget
-    curr_date = datetime.now() 
-    if 8 <= curr_date.month <= 12:  
+###############################################################################
+#Takes in scraped meaplan balance, and calculates the daily budget
+#that person can spend until end of semeseter
+def calculate_daily_spending(meal_plan_balance):
+    curr_date = datetime.now()
+    if 8 <= curr_date.month <= 12:
         end_date = datetime(curr_date.year, 12, 15)
     else:
-        end_date = datetime(curr_date.year, 5, 15) 
-    days_left = (end_date - curr_date).days + 1 
+        end_date = datetime(curr_date.year, 5, 15)
+    days_left = (end_date - curr_date).days + 1
     daily_budget = round((meal_plan_balance / days_left), 2)
-#############################################################
+    return days_left, daily_budget
+###############################################################################
+
+def scrape_recent_transactions(soup, browser):
+    dates = []
+    prices = []
+    ###########################################################################
+    #Finds href link of the recent transactions page using BeautifulSoup, and 
+    #updates the Selenium browser to open transactions page. Since all
+    #transaction are split between multiple pages, the current page and total 
+    #page amounts are scraped and stored in page numbers[] array. This is so we 
+    #can loop through all pages to scrape every transactions
+    transactions_href = soup.find('table', {'width': '500', 'border': '0'}).find('a', href=True).get('href', None)
+    browser.get(f"https://bing.campuscardcenter.com/ch/{transactions_href}")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    page_numbers = soup.find('td', align='center', colspan='7').get_text(strip=True).replace(">>>", '').split(' ')[1].split('/')
+    cur_page = int(page_numbers[0])
+    total_page = int(page_numbers[1])  
+    ##########################################################################
+    
+    ##########################################################################
+    #Loops through every page of transactions using curr_page and total_page 
+    #which were scraped earlier. Scrapes transactions page using BeautifulSoup, 
+    #stores each transactions date and location in dates[] array and stores 
+    #transaction price in prices array. Then, iterateso next page by updating 
+    #Selenium browser with href for next page and repeats.
+    while cur_page <= total_page:      
+        entry_rows = soup.find_all('tr', {'id': 'EntryRow'})
+        for entry_row in entry_rows:
+            try:
+                contents = entry_row.contents
+                date = contents[3].text.strip()
+                dining_hall = contents[7].text.strip()
+                #This condition occurs when money is added to mealplan, 
+                #so its skipped
+                if not dining_hall:
+                    continue
+                price = contents[9].div.text.strip().replace('(', '').replace(')', '')
+                dates.append(f"{date}: {dining_hall}")
+                prices.append(f"${price}")
+            except NoSuchElementException:
+                print("Element not found. Check the HTML structure.")
+        cur_page += 1
+        browser.get(f"https://bing.campuscardcenter.com/ch/{transactions_href}&page={cur_page}")
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+    return dates, prices
+    #########################################################################
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
