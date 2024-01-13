7645c043c2339d68aa160f7b80e60f61a78a8255
Nick DeNobrega <nickdenobrega@gmail.com>
2024-01-12 23:04:44 -0500

    simplified updateStatus Function

--- static/js/food_scripts.js
+++ static/js/food_scripts.js
@@ -39,7 +39,6 @@ document.addEventListener('DOMContentLoaded', function() {
     //updateStatus('c4-food-container');
 });
 
-
 //Updates Open/Closed Status on each food station at dining hall
 function updateStatus(containerId) {
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -48,50 +47,34 @@ function updateStatus(containerId) {
   const currMinute = currDate.getMinutes();
   const currDay = daysOfWeek[currDate.getDay()];
 
+  //Defines a function to take in a time range(Ex: 8:00-10:00), and check if current time is in this range
+  const isFoodStationOpen = (timeRange) => {
+    const [openingTime, closingTime] = timeRange.split('-');
+    const openingHour = parseInt(openingTime.split(':')[0]);
+    const openingMinute = parseInt(openingTime.split(':')[1]);
+    const closingHour = parseInt(closingTime.split(':')[0]);
+    const closingMinute = parseInt(closingTime.split(':')[1]);
+    return (currHour > openingHour || (currHour === openingHour && currMinute >= openingMinute)) &&
+           (currHour < closingHour || (currHour === closingHour && currMinute < closingMinute));
+  };
+
   const foodStations = document.getElementById(containerId).getElementsByClassName('box');
-  for (i = 0; i < foodStations.length; i++) {
-    const foodStation = foodStations[i];
-    const foodStationHours = foodStation.querySelector('.js-hours').innerText.split(', ');
-    const currDaySchedule = foodStationHours.find(dayHours => dayHours.startsWith(currDay));
-    //This if statement will be true when a food station has two times they are open. For example, "11pm-1:30pm and 5pm-8pm"
-    if(currDaySchedule.includes('and')){
-      const [timeRange1, timeRange2] = currDaySchedule.split(': ')[1].split(' and ');
-      const [openingTime1, closingTime1] = timeRange1.split('-');
-      const [openingTime2, closingTime2] = timeRange2.split('-');
-      const openingHour1 = parseInt(openingTime1.split(':')[0]);
-      const openingMinute1 = parseInt(openingTime1.split(':')[1]);
-      const closingHour1 = parseInt(closingTime1.split(':')[0]);
-      const closingMinute1 = parseInt(closingTime1.split(':')[1]);
-      const openingHour2 = parseInt(openingTime2.split(':')[0]);
-      const openingMinute2 = parseInt(openingTime2.split(':')[1]);
-      const closingHour2 = parseInt(closingTime2.split(':')[0]);
-      const closingMinute2 = parseInt(closingTime2.split(':')[1]);
-      //Checks if the current time is in between the opening and closing hour of each time slot of the food station
-      if (((currHour > openingHour1 || (currHour == openingHour1 && currMinute >= openingMinute1)) && (currHour < closingHour1 || (currHour == closingHour1 && currMinute < closingMinute1)))
-      || ((currHour > openingHour2 || (currHour == openingHour2 && currMinute >= openingMinute2)) && (currHour < closingHour2|| (currHour == closingHour2 && currMinute < closingMinute2)))) {
-        foodStation.querySelector('strong').innerText = 'Open';
-        foodStation.querySelector('strong').style.color = '#006747';
-      } else {
-        foodStation.querySelector('strong').innerText = 'Closed';
-        foodStation.querySelector('strong').style.color = 'red';
-      }
+  for (const foodStation of foodStations) {
+    const foodStationHours = foodStation.querySelector('.js-hours').innerText.split(', ').find(stationHours => stationHours.startsWith(currDay));
+    //This if statement will be true when food station has two times its open. For example, "11am-1:30pm and 5pm-8pm"
+    if (foodStationHours.includes('and')) {
+      const [timeRange1, timeRange2] = foodStationHours.split(': ')[1].split(' and ');
+      const isOpen = isFoodStationOpen(timeRange1) || isFoodStationOpen(timeRange2);
+      foodStation.querySelector('strong').innerText = isOpen ? 'Open' : 'Closed';
+      foodStation.querySelector('strong').style.color = isOpen ? '#006747' : 'red';
     } else {
-      const [openingTime, closingTime] = currDaySchedule.split(': ')[1].split('-');
-      const openingHour = parseInt(openingTime.split(':')[0]);
-      const openingMinute = parseInt(openingTime.split(':')[1]);
-      const closingHour = parseInt(closingTime.split(':')[0]);
-      const closingMinute = parseInt(closingTime.split(':')[1]);
-      //Checks if the current time is in between the opening and closing hour of the food station
-      if ((currHour > openingHour || (currHour == openingHour && currMinute >= openingMinute)) 
-      && (currHour < closingHour || (currHour == closingHour && currMinute < closingMinute))) {
-        foodStation.querySelector('strong').innerText = 'Open';
-        foodStation.querySelector('strong').style.color = '#006747';
-      } else {
-        foodStation.querySelector('strong').innerText = 'Closed';
-        foodStation.querySelector('strong').style.color = 'red';
-      }
+      const timeRange = foodStationHours.split(': ')[1];
+      const isOpen = isFoodStationOpen(timeRange);
+      foodStation.querySelector('strong').innerText = isOpen ? 'Open' : 'Closed';
+      foodStation.querySelector('strong').style.color = isOpen ? '#006747' : 'red';
     }
   } 
 }
+
 
 
