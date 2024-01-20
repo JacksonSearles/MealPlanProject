f180bd4a6c3705aaf0d2a2c76be0d17bc515632a
Nick DeNobrega <nickdenobrega@gmail.com>
2024-01-19 19:47:06 -0500

    added scraping menu code for weekends

--- py/food.py
+++ py/food.py
@@ -44,9 +44,26 @@ def get_c4_menu():
                 elif page_number == 6:
                     image.save(os.path.join(folder_path, 'c4_expedition.png'))
             elif current_day in weekends:
-                return None
+                if os.path.exists(os.path.join(folder_path, 'c4_breakfest.png')):
+                    os.remove(os.path.join(folder_path, 'c4_breakfest.png'))
+                if page_number == 0:
+                    image.save(os.path.join(folder_path, 'c4_lunch.png'))
+                elif page_number == 1:
+                    image.save(os.path.join(folder_path, 'c4_dinner.png'))
+                elif page_number == 2:
+                    image.save(os.path.join(folder_path, 'c4_simple_servings_breakfest.png'))
+                elif page_number == 3:
+                    image.save(os.path.join(folder_path, 'c4_simple_servings.png'))
+                elif page_number == 4:
+                    image.save(os.path.join(folder_path, 'c4_grill.png'))
+                elif page_number == 5:
+                    image.save(os.path.join(folder_path, 'c4_pizza.png'))
+                elif page_number == 6:
+                    image.save(os.path.join(folder_path, 'c4_expedition.png'))
+
 
 def get_c4_kosher_menu():
+    days = {'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Sunday'}
     current_day = date.today().strftime('%A')
     folder_path = '/home/bingmealplanhelper/MealPlanProject/static/food_menus/c4-kosher'
     pdf_path = os.path.join(folder_path, 'c4-kosher.pdf')
@@ -68,10 +85,18 @@ def get_c4_kosher_menu():
         for page_number in range(pdf.page_count):
             page = pdf[page_number]
             image = page.get_pixmap()
-            if page_number == 0:
-                image.save(os.path.join(folder_path, 'c4_kosher_lunch.png'))
-            elif page_number == 1:
-                image.save(os.path.join(folder_path, 'c4_kosher_dinner.png'))
+            if current_day in days:
+                if page_number == 0:
+                    image.save(os.path.join(folder_path, 'c4_kosher_lunch.png'))
+                elif page_number == 1:
+                    image.save(os.path.join(folder_path, 'c4_kosher_dinner.png'))
+            elif current_day == 'Friday':
+                if os.path.exists(os.path.join(folder_path, 'c4_kosher_dinner.png')):
+                    os.remove(os.path.join(folder_path, 'c4_kosher_dinner.png'))
+                if page_number == 0:
+                    image.save(os.path.join(folder_path, 'c4_kosher_lunch.png'))
+                  
+
 
 def get_hinman_menu():
     weekdays = {'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'}
@@ -113,7 +138,20 @@ def get_hinman_menu():
                 elif page_number == 6:
                     image.save(os.path.join(folder_path, 'hinman_expedition.png'))
             elif current_day in weekends:
-                return None
+                if os.path.exists(os.path.join(folder_path, 'hinman_breakfest.png')):
+                    os.remove(os.path.join(folder_path, 'hinman_breakfest.png'))
+                if page_number == 0:
+                    image.save(os.path.join(folder_path, 'hinman_lunch.png'))
+                elif page_number == 1:
+                    image.save(os.path.join(folder_path, 'hinman_dinner.png'))
+                elif page_number == 2:
+                    image.save(os.path.join(folder_path, 'hinman_grill.png'))
+                elif page_number == 3:
+                    image.save(os.path.join(folder_path, 'hinman_garden_grill.png'))
+                elif page_number == 4:
+                    image.save(os.path.join(folder_path, 'hinman_pizza.png'))
+                elif page_number == 5:
+                    image.save(os.path.join(folder_path, 'hinman_expedition.png'))
 
 
 def get_app_menu():
@@ -156,7 +194,25 @@ def get_app_menu():
                 elif page_number == 6:
                     image.save(os.path.join(folder_path, 'app_special_soup.png'))
             elif current_day in weekends:
-                return None    
+                if os.path.exists(os.path.join(folder_path, 'app_breakfest.png')):
+                    os.remove(os.path.join(folder_path, 'app_breakfest.png'))  
+                elif page_number == 0:
+                    image.save(os.path.join(folder_path, 'app_lunch.png'))
+                elif page_number == 1:
+                    image.save(os.path.join(folder_path, 'app_dinner.png'))
+                elif page_number == 2:
+                    image.save(os.path.join(folder_path, 'app_simple_servings_breakfest.png'))
+                elif page_number == 3:
+                    image.save(os.path.join(folder_path, 'app_simple_servings.png'))
+                elif page_number == 4:
+                    image.save(os.path.join(folder_path, 'app_grill.png'))
+                elif page_number == 5:
+                    image.save(os.path.join(folder_path, 'app_pizza.png'))
+                elif page_number == 6:
+                    image.save(os.path.join(folder_path, 'app_special_soup_breakfest.png'))
+                elif page_number == 7:
+                    image.save(os.path.join(folder_path, 'app_special_soup.png'))
+                  
 
 def get_ciw_menu():
     weekdays = {'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'}
@@ -193,6 +249,10 @@ def get_ciw_menu():
                 elif page_number == 4:
                     image.save(os.path.join(folder_path, 'ciw_ultimate.png'))
             elif current_day == 'Friday':
+                if os.path.exists(os.path.join(folder_path, 'ciw_dinner.png')):
+                    os.remove(os.path.join(folder_path, 'ciw_dinner.png'))
+                if os.path.exists(os.path.join(folder_path, 'ciw_ultimate.png')):
+                    os.remove(os.path.join(folder_path, 'ciw_ultimate.png'))
                 if page_number == 0:
                     image.save(os.path.join(folder_path, 'ciw_breakfest.png'))
                 elif page_number == 1:
@@ -200,7 +260,19 @@ def get_ciw_menu():
                 elif page_number == 2:
                     image.save(os.path.join(folder_path, 'ciw_pizza.png'))
             else:
-                return      
+                if os.path.exists(os.path.join(folder_path, 'ciw_breakfest.png')):
+                    os.remove(os.path.join(folder_path, 'ciw_breakfest.png'))
+                if os.path.exists(os.path.join(folder_path, 'ciw_lunch.png')):
+                    os.remove(os.path.join(folder_path, 'ciw_lunch.png'))
+                if os.path.exists(os.path.join(folder_path, 'ciw_dinner.png')):
+                    os.remove(os.path.join(folder_path, 'ciw_dinner.png'))
+                if os.path.exists(os.path.join(folder_path, 'ciw_pizza.png')):
+                    os.remove(os.path.join(folder_path, 'ciw_pizza.png'))
+                if os.path.exists(os.path.join(folder_path, 'ciw_pizza.png')):
+                    os.remove(os.path.join(folder_path, 'ciw_pizza.png'))
+                if os.path.exists(os.path.join(folder_path, 'ciw_ultimate.png')):
+                    os.remove(os.path.join(folder_path, 'ciw_ultimate.png'))
+
 
 get_c4_menu()
 get_c4_kosher_menu()
